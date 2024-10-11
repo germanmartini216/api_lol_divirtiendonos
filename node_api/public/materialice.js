e41306eeea2a12a96d76892d21a5d17535e8b58a
LucioBxll <lucio.boxall15@gmail.com>
2024-10-10 21:47:26 -0300

    cambios

--- node_api/public/materialice.js
+++ node_api/public/materialice.js
@@ -1,4 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Inicializar el efecto parallax
     var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, 0);
-  });
+    var instances = M.Parallax.init(elems);
+
+    // Seleccionar el botón de scroll hacia arriba
+    const scrollToTopButton = document.querySelector('.fixed-action-btn-right');
+
+    // Evento de scroll para mostrar/ocultar el botón
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 100) { // Cambia 100 por el valor que prefieras
+            scrollToTopButton.classList.add('show');
+        } else {
+            scrollToTopButton.classList.remove('show');
+        }
+    });
+
+    // Evento de clic para desplazamiento suave hacia arriba
+    scrollToTopButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
