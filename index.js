ef58d0ebe20c0c514f07d8e0a91026133aca012a
LucioBxll <lucio.boxall15@gmail.com>
2024-11-19 21:07:34 -0300

    Version Final

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 import champsRoutes from './routes/champsRoutes.js';
 import usersRoutes from './routes/usersRoute.js';
 import mapsRoutes from './routes/mapsRoutes.js';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +17,8 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Conectado a MongoDB'))
 .catch(err => console.error('Error al conectar a MongoDB', err));
 
-const app = express();
+const app = express(); 
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,6 +32,5 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-});
+app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
+
